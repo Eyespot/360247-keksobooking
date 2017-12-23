8eded60e4d5f5e9c57c9bb5702d44e30566efa91
Eyespot <eger_np_@inbox.ru>
2017-12-23 21:14:09 +0300

    http connection timeout value changed

--- js/backend.js
+++ js/backend.js
@@ -4,7 +4,7 @@
 (function () {
   var SERVER_URL = 'https://1510.dump.academy/keksobooking';
   var OK_STATUS = 200;
-  var HTTP_CONNECT_TIMEOUT = 2000;
+  var HTTP_CONNECT_TIMEOUT = 20000;
 
   var HTTP_ERRORS = {
     400: 'Неверный запрос.',
